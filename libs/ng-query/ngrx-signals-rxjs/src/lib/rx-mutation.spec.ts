91ece387daf489a0f1fa19e68714298eca18a740
Romain <romain.geffrault@gmail.com>
2025-09-05 14:59:30 +0200

    add rx mutation insertions

--- libs/ng-query/ngrx-signals-rxjs/src/lib/rx-mutation.spec.ts
+++ libs/ng-query/ngrx-signals-rxjs/src/lib/rx-mutation.spec.ts
@@ -3,6 +3,7 @@ import { signalStore } from '@ngrx/signals';
 import { TestBed } from '@angular/core/testing';
 import { withMutation } from '@ng-query/ngrx-signals';
 import { rxMutation } from './rx-mutation';
+import { inject } from '@angular/core';
 
 type User = {
   id: string;
@@ -102,5 +103,146 @@ describe('withMutation using rxMutation', () => {
     const store = TestBed.inject(Store);
 
     expect(store.userMutation).toBeDefined();
+  });
+
+  it('should accept an insertion output, that appear in the store', () => {
+    const Store = signalStore(
+      {
+        providedIn: 'root',
+      },
+      withMutation('user', () =>
+        rxMutation(
+          {
+            params: () => '5',
+            stream: ({ params }) => {
+              return of({
+                id: params,
+                name: 'John Doe',
+                email: 'test@a.com',
+              });
+            },
+          },
+          (data) => {
+            console.log('data', data);
+            return {
+              pagination: {
+                page: 1,
+              },
+            };
+          }
+        )
+      )
+    );
+    TestBed.runInInjectionContext(() => {
+      const store = inject(Store);
+      expectTypeOf(store.userMutation.pagination).toEqualTypeOf<{
+        page: number;
+      }>();
+      expect(store.userMutation.pagination).toBeDefined();
+    });
+  });
+  it('should accept multiple insertions, that appear in the store', () => {
+    const Store = signalStore(
+      {
+        providedIn: 'root',
+      },
+      withMutation('user', () =>
+        rxMutation(
+          {
+            params: () => '5',
+            stream: ({ params }) => {
+              return of({
+                id: params,
+                name: 'John Doe',
+                email: 'test@a.com',
+              });
+            },
+          },
+          // insert 1
+          () => {
+            return {
+              pagination: {
+                page: 1,
+              },
+            };
+          },
+          // insert 2
+          ({ insertions: inserts }) => {
+            expectTypeOf(inserts).toEqualTypeOf<{
+              pagination: {
+                page: number;
+              };
+            }>();
+            return {
+              someOtherInfo: true,
+            };
+          }
+        )
+      )
+    );
+    TestBed.runInInjectionContext(() => {
+      const store = inject(Store);
+      //insert 1
+      expectTypeOf(store.userMutation.pagination).toEqualTypeOf<{
+        page: number;
+      }>();
+      expect(store.userMutation.pagination).toBeDefined();
+
+      //insert 2
+      expectTypeOf(store.userMutation.someOtherInfo).toEqualTypeOf<boolean>();
+      expect(store.userMutation.someOtherInfo).toBeDefined();
+    });
+  });
+  it('should accept seven insertions, all outputs appear in the store', () => {
+    const Store = signalStore(
+      {
+        providedIn: 'root',
+      },
+      withMutation('user', () =>
+        rxMutation(
+          {
+            params: () => '5',
+            stream: ({ params }) => {
+              return of({
+                id: params,
+                name: 'John Doe',
+                email: 'test@a.com',
+              });
+            },
+          },
+          // insert 1
+          () => ({ ext1: 1 }),
+          // insert 2
+          ({ insertions: inserts }) => ({ ext2: inserts.ext1 + 1 }),
+          // insert 3
+          ({ insertions: inserts }) => ({ ext3: inserts.ext2 + 1 }),
+          // insert 4
+          ({ insertions: inserts }) => ({ ext4: inserts.ext3 + 1 }),
+          // insert 5
+          ({ insertions: inserts }) => ({ ext5: inserts.ext4 + 1 }),
+          // insert 6
+          ({ insertions: inserts }) => ({ ext6: inserts.ext5 + 1 }),
+          // insert 7
+          ({ insertions: inserts }) => ({ ext7: inserts.ext6 + 1 })
+        )
+      )
+    );
+    TestBed.runInInjectionContext(() => {
+      const store = inject(Store);
+      expectTypeOf(store.userMutation.ext1).toEqualTypeOf<number>();
+      expectTypeOf(store.userMutation.ext2).toEqualTypeOf<number>();
+      expectTypeOf(store.userMutation.ext3).toEqualTypeOf<number>();
+      expectTypeOf(store.userMutation.ext4).toEqualTypeOf<number>();
+      expectTypeOf(store.userMutation.ext5).toEqualTypeOf<number>();
+      expectTypeOf(store.userMutation.ext6).toEqualTypeOf<number>();
+      expectTypeOf(store.userMutation.ext7).toEqualTypeOf<number>();
+      expect(store.userMutation.ext1).toBeDefined();
+      expect(store.userMutation.ext2).toBeDefined();
+      expect(store.userMutation.ext3).toBeDefined();
+      expect(store.userMutation.ext4).toBeDefined();
+      expect(store.userMutation.ext5).toBeDefined();
+      expect(store.userMutation.ext6).toBeDefined();
+      expect(store.userMutation.ext7).toBeDefined();
+    });
   });
 });
--- libs/ng-query/ngrx-signals-rxjs/src/lib/rx-mutation.ts
+++ libs/ng-query/ngrx-signals-rxjs/src/lib/rx-mutation.ts
@@ -1,13 +1,41 @@
-import {
-  SignalStoreFeatureResult,
-  Prettify,
-  StateSignals,
-} from '@ngrx/signals';
+import { SignalStoreFeatureResult } from '@ngrx/signals';
 import { RxResourceWithParamsOrParamsFn } from './types/rx-resource-with-params-or-params-fn.type';
-import { InternalType, PublicSignalStore } from '@ng-query/ngrx-signals';
-import { signal, WritableSignal } from '@angular/core';
+import {
+  InsertionsFactory,
+  InternalType,
+  PublicSignalStore,
+} from '@ng-query/ngrx-signals';
+import { ResourceRef, signal, WritableSignal } from '@angular/core';
 import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { MutationRef } from '@ng-query/ngrx-signals';
+
+type RxMutationOutput<
+  StoreInput extends PublicSignalStore<Input>,
+  Input extends SignalStoreFeatureResult,
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Insertions
+> = (
+  store: StoreInput,
+  context: Input
+) => {
+  mutationRef: MutationRef<
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    NoInfer<MutationArgsParams>,
+    Insertions
+  >;
+  /**
+   * Only used to help type inference, not used in the actual implementation.
+   */
+  __types: InternalType<
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    NoInfer<MutationArgsParams>,
+    false
+  >;
+};
 
 export function rxMutation<
   MutationState extends object | undefined,
@@ -21,25 +49,426 @@ export function rxMutation<
     MutationParams,
     MutationArgsParams
   >
-): (
-  store: StoreInput,
-  context: Input
-) => {
-  mutationRef: MutationRef<
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  {}
+>;
+export function rxMutation<
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Input extends SignalStoreFeatureResult,
+  const StoreInput extends PublicSignalStore<Input>,
+  Insertion1
+>(
+  mutationConfig: RxResourceWithParamsOrParamsFn<
+    MutationState,
+    MutationParams,
+    MutationArgsParams
+  >,
+  insertion1: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
     NoInfer<MutationState>,
     NoInfer<MutationParams>,
-    NoInfer<MutationArgsParams>
-  >;
-  /**
-   * Only used to help type inference, not used in the actual implementation.
-   */
-  __types: InternalType<
+    Insertion1
+  >
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  Insertion1
+>;
+export function rxMutation<
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Input extends SignalStoreFeatureResult,
+  const StoreInput extends PublicSignalStore<Input>,
+  Insertion1,
+  Insertion2
+>(
+  mutationConfig: RxResourceWithParamsOrParamsFn<
+    MutationState,
+    MutationParams,
+    MutationArgsParams
+  >,
+  insertion1: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
     NoInfer<MutationState>,
     NoInfer<MutationParams>,
-    NoInfer<MutationArgsParams>,
-    false
-  >;
-} {
+    Insertion1
+  >,
+  insertion2: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion2,
+    Insertion1
+  >
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  Insertion1 & Insertion2
+>;
+export function rxMutation<
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Input extends SignalStoreFeatureResult,
+  const StoreInput extends PublicSignalStore<Input>,
+  Insertion1,
+  Insertion2,
+  Insertion3
+>(
+  mutationConfig: RxResourceWithParamsOrParamsFn<
+    MutationState,
+    MutationParams,
+    MutationArgsParams
+  >,
+  insertion1: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion1
+  >,
+  insertion2: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion2,
+    Insertion1
+  >,
+  insertion3: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion3,
+    Insertion1 & Insertion2
+  >
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  Insertion1 & Insertion2 & Insertion3
+>;
+export function rxMutation<
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Input extends SignalStoreFeatureResult,
+  const StoreInput extends PublicSignalStore<Input>,
+  Insertion1,
+  Insertion2,
+  Insertion3,
+  Insertion4
+>(
+  mutationConfig: RxResourceWithParamsOrParamsFn<
+    MutationState,
+    MutationParams,
+    MutationArgsParams
+  >,
+  insertion1: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion1
+  >,
+  insertion2: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion2,
+    Insertion1
+  >,
+  insertion3: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion3,
+    Insertion1 & Insertion2
+  >,
+  insertion4: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion4,
+    Insertion1 & Insertion2 & Insertion3
+  >
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  Insertion1 & Insertion2 & Insertion3 & Insertion4
+>;
+export function rxMutation<
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Input extends SignalStoreFeatureResult,
+  const StoreInput extends PublicSignalStore<Input>,
+  Insertion1,
+  Insertion2,
+  Insertion3,
+  Insertion4,
+  Insertion5
+>(
+  mutationConfig: RxResourceWithParamsOrParamsFn<
+    MutationState,
+    MutationParams,
+    MutationArgsParams
+  >,
+  insertion1: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion1
+  >,
+  insertion2: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion2,
+    Insertion1
+  >,
+  insertion3: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion3,
+    Insertion1 & Insertion2
+  >,
+  insertion4: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion4,
+    Insertion1 & Insertion2 & Insertion3
+  >,
+  insertion5: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion5,
+    Insertion1 & Insertion2 & Insertion3 & Insertion4
+  >
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  Insertion1 & Insertion2 & Insertion3 & Insertion4 & Insertion5
+>;
+export function rxMutation<
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Input extends SignalStoreFeatureResult,
+  const StoreInput extends PublicSignalStore<Input>,
+  Insertion1,
+  Insertion2,
+  Insertion3,
+  Insertion4,
+  Insertion5,
+  Insertion6
+>(
+  mutationConfig: RxResourceWithParamsOrParamsFn<
+    MutationState,
+    MutationParams,
+    MutationArgsParams
+  >,
+  insertion1: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion1
+  >,
+  insertion2: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion2,
+    Insertion1
+  >,
+  insertion3: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion3,
+    Insertion1 & Insertion2
+  >,
+  insertion4: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion4,
+    Insertion1 & Insertion2 & Insertion3
+  >,
+  insertion5: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion5,
+    Insertion1 & Insertion2 & Insertion3 & Insertion4
+  >,
+  insertion6: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion6,
+    Insertion1 & Insertion2 & Insertion3 & Insertion4 & Insertion5
+  >
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  Insertion1 & Insertion2 & Insertion3 & Insertion4 & Insertion5 & Insertion6
+>;
+export function rxMutation<
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Input extends SignalStoreFeatureResult,
+  const StoreInput extends PublicSignalStore<Input>,
+  Insertion1,
+  Insertion2,
+  Insertion3,
+  Insertion4,
+  Insertion5,
+  Insertion6,
+  Insertion7
+>(
+  mutationConfig: RxResourceWithParamsOrParamsFn<
+    MutationState,
+    MutationParams,
+    MutationArgsParams
+  >,
+  insertion1: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion1
+  >,
+  insertion2: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion2,
+    Insertion1
+  >,
+  insertion3: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion3,
+    Insertion1 & Insertion2
+  >,
+  insertion4: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion4,
+    Insertion1 & Insertion2 & Insertion3
+  >,
+  insertion5: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion5,
+    Insertion1 & Insertion2 & Insertion3 & Insertion4
+  >,
+  insertion6: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion6,
+    Insertion1 & Insertion2 & Insertion3 & Insertion4 & Insertion5
+  >,
+  insertion7: InsertionsFactory<
+    NoInfer<Input>,
+    NoInfer<StoreInput>,
+    NoInfer<MutationState>,
+    NoInfer<MutationParams>,
+    Insertion7,
+    Insertion1 & Insertion2 & Insertion3 & Insertion4 & Insertion5 & Insertion6
+  >
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  Insertion1 &
+    Insertion2 &
+    Insertion3 &
+    Insertion4 &
+    Insertion5 &
+    Insertion6 &
+    Insertion7
+>;
+export function rxMutation<
+  MutationState extends object | undefined,
+  MutationParams,
+  MutationArgsParams,
+  Input extends SignalStoreFeatureResult,
+  const StoreInput extends PublicSignalStore<Input>
+>(
+  mutationConfig: RxResourceWithParamsOrParamsFn<
+    MutationState,
+    MutationParams,
+    MutationArgsParams
+  >,
+  ...insertions: any[]
+): RxMutationOutput<
+  StoreInput,
+  Input,
+  MutationState,
+  MutationParams,
+  MutationArgsParams,
+  {}
+> {
   const src$ = mutationConfig.params$;
   const src$ToSignal = src$ ? toSignal(src$) : undefined;
   const mutationResourceParamsFnSignal = signal<MutationParams | undefined>(
@@ -61,6 +490,28 @@ export function rxMutation<
         MutationParams | undefined
       >,
       method: mutationConfig.method,
+      insertionsOutputs: (
+        insertions as InsertionsFactory<
+          NoInfer<Input>,
+          NoInfer<StoreInput>,
+          NoInfer<MutationState>,
+          NoInfer<MutationParams>,
+          {}
+        >[]
+      )?.reduce((acc, insert) => {
+        return {
+          ...acc,
+          ...insert({
+            input: context,
+            store,
+            resource: mutationResource as ResourceRef<MutationState>,
+            resourceParams: resourceParamsSrc as WritableSignal<
+              NoInfer<MutationParams>
+            >,
+            insertions: acc as {},
+          }),
+        };
+      }, {} as Record<string, unknown>),
     },
     __types: {} as InternalType<
       NoInfer<MutationState>,
