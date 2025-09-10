f30b784f065bc22217d8319c7c18b8e58f654843
Romain <romain.geffrault@gmail.com>
2025-09-10 08:09:25 +0200

    index

--- libs/ng-query/core/schematics/ng-add/index.ts
+++ libs/ng-query/core/schematics/ng-add/index.ts
@@ -1,43 +1,34 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import { addRootImport } from '@schematics/angular/utility';
 
 export interface Schema {
   useRxjs?: boolean;
 }
 
-export function ngAdd(options: Schema): Rule {
-  // Add an import `MyLibModule` from `my-lib` to the root of the user's project.
-  return addRootImport(
-    'test',
-    ({ code, external }) => code`${external('MyLibModule', 'my-lib')}`
-  );
+export default function (options: Schema): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.addTask(
+      new NodePackageInstallTask({
+        packageName: '@ng-query/ngrx-signals',
+      })
+    );
+    context.addTask(
+      new NodePackageInstallTask({
+        packageName: '@ng-query/insertions',
+      })
+    );
+    if (options.useRxjs) {
+      context.addTask(
+        new NodePackageInstallTask({
+          packageName: '@ng-query/ngrx-signals-rxjs',
+        })
+      );
+      context.addTask(
+        new NodePackageInstallTask({
+          packageName: '@ng-query/rx-insertions',
+        })
+      );
+    }
+    return tree;
+  };
 }
-
-// export default function ngAdd(options: Schema): Rule {
-//   return (tree: Tree, context: SchematicContext) => {
-//     context.addTask(
-//       new NodePackageInstallTask({
-//         packageName: '@ng-query/ngrx-signals',
-//       })
-//     );
-//     context.addTask(
-//       new NodePackageInstallTask({
-//         packageName: '@ng-query/insertions',
-//       })
-//     );
-//     if (options.useRxjs) {
-//       context.addTask(
-//         new NodePackageInstallTask({
-//           packageName: '@ng-query/ngrx-signals-rxjs',
-//         })
-//       );
-//       context.addTask(
-//         new NodePackageInstallTask({
-//           packageName: '@ng-query/rx-insertions',
-//         })
-//       );
-//     }
-//     return tree;
-//   };
-// }
