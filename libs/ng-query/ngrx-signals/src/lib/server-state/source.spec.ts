07d4e5d6608545892724bd00656926cc2e670658
Romain <rgeffrault@dood.com>
2025-10-26 06:50:11 +0100

    add source signal

---
+++ libs/ng-query/ngrx-signals/src/lib/server-state/source.spec.ts
@@ -0,0 +1,53 @@
+import { computed } from '@angular/core';
+import { source } from './source';
+
+describe('source', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+  it('should generate a source that enable to emit a value, and the listener to receive it', () => {
+    const mySource = source<string>();
+
+    const myListener = computed(() => {
+      const s = mySource();
+      return s;
+    });
+
+    expect(myListener()).toBe(undefined);
+
+    mySource.set('Hello World');
+
+    expect(myListener()).toBe('Hello World');
+
+    mySource.set('Hello Ng-Query');
+    expect(myListener()).toBe('Hello Ng-Query');
+  });
+
+  it('A listener at n+1 should not get the value when listened and get data for the first time', () => {
+    const mySource = source<string>();
+
+    mySource.set('Hello World');
+
+    const myListener = computed(() => mySource());
+    expect(myListener()).toBe(undefined);
+
+    mySource.set('Hello Ng-Query v2');
+    expect(myListener()).toBe('Hello Ng-Query v2');
+  });
+
+  it('A listener at n+1 should get the last value when using "preserveLastValue" config and listened and get data for the first time ', () => {
+    const mySource = source<string>();
+
+    mySource.set('Hello World');
+
+    const myListener = computed(() => mySource.preserveLastValue());
+    expect(myListener()).toBe('Hello World');
+
+    mySource.set('Hello Ng-Query v2');
+    expect(myListener()).toBe('Hello Ng-Query v2');
+  });
+});
---
+++ libs/ng-query/ngrx-signals/src/lib/server-state/source.ts
@@ -0,0 +1,46 @@
+import { linkedSignal, Signal, signal, ValueEqualityFn } from '@angular/core';
+
+type Source<T> = Signal<T | undefined> & {
+  set: (value: T) => void;
+  preserveLastValue: Signal<T | undefined>;
+};
+
+//todo stackSource
+
+export function source<T>(options?: {
+  equal?: ValueEqualityFn<NoInfer<T> | undefined>;
+  debugName?: string;
+}): Source<T> {
+  const sourceState = signal<T | undefined>(undefined, {
+    ...(options?.equal && { equal: options?.equal }), // add the equal function here, it may helps to detect changes when using scalar values
+    ...(options?.debugName && {
+      debugName: options?.debugName + '_sourceState',
+    }),
+  });
+
+  const listener = (listenerOptions: { nullishFirstValue?: boolean }) =>
+    linkedSignal<T, T | undefined>({
+      source: sourceState as Signal<T>,
+      computation: (currentSourceState, previousData) => {
+        // always when first listened return undefined
+        if (!previousData && listenerOptions?.nullishFirstValue !== false) {
+          return undefined;
+        }
+
+        return currentSourceState;
+      },
+      ...(options?.equal && { equal: options?.equal }),
+      ...(options?.debugName && { debugName: options?.debugName }),
+    });
+  return Object.assign(
+    listener({
+      nullishFirstValue: true,
+    }),
+    {
+      preserveLastValue: listener({
+        nullishFirstValue: false,
+      }),
+      set: sourceState.set,
+    }
+  ) as Source<T>;
+}
