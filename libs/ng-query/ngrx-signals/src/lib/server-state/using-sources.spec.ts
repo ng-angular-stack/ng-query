3f5510f0776db70796249468823c4245f04eb514
Romain <rgeffrault@dood.com>
2025-11-07 18:55:22 +0100

    remove useless

--- libs/ng-query/ngrx-signals/src/lib/server-state/using-sources.spec.ts
+++ libs/ng-query/ngrx-signals/src/lib/server-state/using-sources.spec.ts
@@ -1,50 +1,12 @@
-import { usingQuery } from './using-query';
 import { serverState } from './server-state';
-import { query } from '../query';
-import { usingInputs } from './using-inputs';
-import { ApplicationRef, computed, effect, signal } from '@angular/core';
+import { ApplicationRef, signal } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { usingSources } from './using-sources';
-import { source, Source } from './source';
-import { toSource } from './to-source';
-import { computedSource } from './computed-source';
+import { source } from './source';
 import { usingState } from './using-state';
 import { on } from './on';
 
 describe('usingSources', () => {
-  // it('1- Should expose a way to set serverState inputs', async () => {
-  //   await TestBed.runInInjectionContext(async () => {
-  //     const { injectServerState } = serverState(
-  //       usingSources({
-  //         params: source<string>(),
-  //         reset: source<string>(),
-  //       }),
-  //     );
-  //     const myParamsInput = signal('1');
-  //     const paramsSource = signal('1');
-  //     const resetSource = source<{ id: string }>();
-  //     const store = injectServerState({
-  //       myParams: myParamsInput, // required
-  //       connectParamsSourceTo: computedSource(
-  //         resetSource,
-  //         (sourceValue) => sourceValue.id
-  //       ),
-  //       // params: paramsSource, // not required
-  //       // connectResetSourceTo: (resetSourceValue) => resetSourceValue.id,
-  //     });
-
-  //     store.setParams('1');
-
-  //     expect(store.userQuery).toBeDefined();
-  //     await vi.runAllTimersAsync();
-  //     expect(store.userQuery.value()).toEqual({
-  //       id: '1',
-  //       name: 'John Doe',
-  //       email: 'test@a.com',
-  //     });
-  //   });
-  // });
-
   beforeEach(() => {
     vi.useFakeTimers();
   });
