f16878203af7144bf3940085051005fca84ef8f9
Romain <rgeffrault@dood.com>
2025-11-08 11:42:11 +0100

    feat: add tests for async methods with local and global sources

--- libs/ng-query/ngrx-signals/src/lib/server-state/using-async-methods.spec.ts
+++ libs/ng-query/ngrx-signals/src/lib/server-state/using-async-methods.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { serverState } from './server-state';
 import { usingAsyncMethods } from './using-async-methods';
 import { asyncMethod } from './async-method';
+import { source } from './source';
+import { on } from './on';
+import { usingSources } from './using-sources';
 
 // todo async methods and query/mutations should expose source
 // todo penser aux //asyncMethodsById
@@ -13,7 +16,7 @@ describe('usingAsyncMethods', () => {
   afterEach(() => {
     vi.resetAllMocks();
   });
-  it('should enable to define async methods that can update the state', async () => {
+  it('should enable to define async method', async () => {
     await TestBed.runInInjectionContext(async () => {
       const { injectServerState } = serverState(
         usingAsyncMethods(() => ({
@@ -48,6 +51,65 @@ describe('usingAsyncMethods', () => {
       await vi.runAllTimersAsync();
       expect(store.searchChange.status()).toBe('resolved');
       expect(store.searchChange.value()).toEqual({ searchChange: 'test' });
+    });
+  });
+
+  it('should enable to define async methods bind to a local and global source', async () => {
+    await TestBed.runInInjectionContext(async () => {
+      const myGlobalSource = source<{
+        timeToWait: number;
+        searchChange: string;
+      }>();
+      const { injectServerState } = serverState(
+        usingSources({
+          myLocalSource: source<{
+            timeToWait: number;
+            searchChange: string;
+          }>(),
+        }),
+        usingAsyncMethods(({ myLocalSource }) => ({
+          searchGlobalChange: asyncMethod({
+            method: on(myGlobalSource, (payload) => payload),
+            loader: async ({ params: { timeToWait, searchChange } }) => {
+              await new Promise((resolve) => setTimeout(resolve, timeToWait));
+              return { searchChange };
+            },
+          }),
+          searchLocalChange: asyncMethod({
+            method: on(myLocalSource, (payload) => payload),
+            loader: async ({ params: { timeToWait, searchChange } }) => {
+              await new Promise((resolve) => setTimeout(resolve, timeToWait));
+              return { searchChange };
+            },
+          }),
+        }))
+      );
+      const store = injectServerState();
+      expect(store.searchGlobalChange.status()).toBe('idle');
+      myGlobalSource.set({
+        searchChange: 'global',
+        timeToWait: 1000,
+      });
+      await vi.advanceTimersByTimeAsync(500);
+      expect(store.searchGlobalChange.status()).toBe('loading');
+      await vi.runAllTimersAsync();
+      expect(store.searchGlobalChange.status()).toBe('resolved');
+      expect(store.searchGlobalChange.value()).toEqual({
+        searchChange: 'global',
+      });
+
+      expect(store.searchLocalChange.status()).toBe('idle');
+      store.setMyLocalSource({
+        searchChange: 'local',
+        timeToWait: 1000,
+      });
+      await vi.advanceTimersByTimeAsync(500);
+      expect(store.searchLocalChange.status()).toBe('loading');
+      await vi.runAllTimersAsync();
+      expect(store.searchLocalChange.status()).toBe('resolved');
+      expect(store.searchLocalChange.value()).toEqual({
+        searchChange: 'local',
+      });
     });
   });
 });
