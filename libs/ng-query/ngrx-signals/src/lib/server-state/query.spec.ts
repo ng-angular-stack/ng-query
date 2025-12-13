266f5413d5bd39edddcf2e68ddfccd1d3463226b
Romain <rgeffrault@dood.com>
2025-12-13 19:17:01 +0100

    refactor: standardize query insertion tests for consistency and clarity

--- libs/ng-query/ngrx-signals/src/lib/server-state/query.spec.ts
+++ libs/ng-query/ngrx-signals/src/lib/server-state/query.spec.ts
@@ -91,183 +91,183 @@ describe('withQuery using query', () => {
 
     expect(store.userQuery).toBeDefined();
   });
+});
 
-  describe('query Insertions output', () => {
-    it('should accept an Insertions output, that appear in the store', () => {
-      const { injectCraft } = craft(
-        {
-          name: '',
-          providedIn: 'root',
-        },
-        craftQuery('user', () =>
-          query(
-            {
-              params: () => '5',
-              loader: async ({ params }) => {
-                return {
-                  id: params,
-                  name: 'John Doe',
-                  email: 'test@a.com',
-                };
-              },
+describe('query Insertions output', () => {
+  it('should accept an Insertions output, that appear in the store', () => {
+    const { injectCraft } = craft(
+      {
+        name: '',
+        providedIn: 'root',
+      },
+      craftQuery('user', () =>
+        query(
+          {
+            params: () => '5',
+            loader: async ({ params }) => {
+              return {
+                id: params,
+                name: 'John Doe',
+                email: 'test@a.com',
+              };
             },
-            () => ({
+          },
+          () => ({
+            pagination: {
+              page: 1,
+            },
+          })
+        )
+      )
+    );
+    TestBed.runInInjectionContext(() => {
+      const store = injectCraft();
+      expect(store.userQuery.pagination).toEqual({ page: 1 });
+      expect(store.userQuery.pagination).toBeDefined();
+    });
+  });
+
+  it('should accept an insertion output, that appear in the store', () => {
+    const { injectCraft } = craft(
+      {
+        name: '',
+        providedIn: 'root',
+      },
+      craftQuery('user', () =>
+        query(
+          {
+            params: () => '5',
+            loader: async ({ params }) => {
+              return {
+                id: params,
+                name: 'John Doe',
+                email: 'test@a.com',
+              } satisfies User;
+            },
+          },
+          (data) => {
+            console.log('data', data);
+            return {
               pagination: {
                 page: 1,
               },
-            })
-          )
+            };
+          }
         )
-      );
-      TestBed.runInInjectionContext(() => {
-        const store = injectCraft();
-        expect(store.userQuery.pagination).toEqual({ page: 1 });
-        expect(store.userQuery.pagination).toBeDefined();
-      });
+      )
+    );
+    TestBed.runInInjectionContext(() => {
+      const store = injectCraft();
+      expectTypeOf(store.userQuery.pagination).toEqualTypeOf<{
+        page: number;
+      }>();
+      expect(store.userQuery.pagination).toBeDefined();
     });
+  });
+  it('should accept multiple insertions, that appear in the store', () => {
+    const { injectCraft } = craft(
+      {
+        name: '',
+        providedIn: 'root',
+      },
+      craftQuery('user', () =>
+        query(
+          {
+            params: () => '5',
+            loader: async ({ params }) => {
+              return {
+                id: params,
+                name: 'John Doe',
+                email: 'test@a.com',
+              } satisfies User;
+            },
+          },
+          // insert 1
+          () => {
+            return {
+              pagination: {
+                page: 1,
+              },
+            };
+          },
+          // insert 2
+          ({ insertions: inserts }) => {
+            expectTypeOf(inserts).toEqualTypeOf<{
+              pagination: {
+                page: number;
+              };
+            }>();
+            return {
+              someOtherInfo: true,
+            };
+          }
+        )
+      )
+    );
+    TestBed.runInInjectionContext(() => {
+      const store = injectCraft();
+      //insert 1
+      expectTypeOf(store.userQuery.pagination).toEqualTypeOf<{
+        page: number;
+      }>();
+      expect(store.userQuery.pagination).toBeDefined();
 
-    it('should accept an insertion output, that appear in the store', () => {
-      const { injectCraft } = craft(
-        {
-          name: '',
-          providedIn: 'root',
-        },
-        craftQuery('user', () =>
-          query(
-            {
-              params: () => '5',
-              loader: async ({ params }) => {
-                return {
-                  id: params,
-                  name: 'John Doe',
-                  email: 'test@a.com',
-                } satisfies User;
-              },
-            },
-            (data) => {
-              console.log('data', data);
-              return {
-                pagination: {
-                  page: 1,
-                },
-              };
-            }
-          )
-        )
-      );
-      TestBed.runInInjectionContext(() => {
-        const store = injectCraft();
-        expectTypeOf(store.userQuery.pagination).toEqualTypeOf<{
-          page: number;
-        }>();
-        expect(store.userQuery.pagination).toBeDefined();
-      });
+      //insert 2
+      expectTypeOf(store.userQuery.someOtherInfo).toEqualTypeOf<boolean>();
+      expect(store.userQuery.someOtherInfo).toBeDefined();
     });
-    it('should accept multiple insertions, that appear in the store', () => {
-      const { injectCraft } = craft(
-        {
-          name: '',
-          providedIn: 'root',
-        },
-        craftQuery('user', () =>
-          query(
-            {
-              params: () => '5',
-              loader: async ({ params }) => {
-                return {
-                  id: params,
-                  name: 'John Doe',
-                  email: 'test@a.com',
-                } satisfies User;
-              },
-            },
-            // insert 1
-            () => {
+  });
+  it('should accept seven insertions, all outputs appear in the store', () => {
+    const { injectCraft } = craft(
+      {
+        name: '',
+        providedIn: 'root',
+      },
+      craftQuery('user', () =>
+        query(
+          {
+            params: () => '5',
+            loader: async ({ params }) => {
               return {
-                pagination: {
-                  page: 1,
-                },
-              };
+                id: params,
+                name: 'John Doe',
+                email: 'test@a.com',
+              } satisfies User;
             },
-            // insert 2
-            ({ insertions: inserts }) => {
-              expectTypeOf(inserts).toEqualTypeOf<{
-                pagination: {
-                  page: number;
-                };
-              }>();
-              return {
-                someOtherInfo: true,
-              };
-            }
-          )
+          },
+          // insert 1
+          () => ({ ext1: 1 }),
+          // insert 2
+          ({ insertions: inserts }) => ({ ext2: inserts.ext1 + 1 }),
+          // insert 3
+          ({ insertions: inserts }) => ({ ext3: inserts.ext2 + 1 }),
+          // insert 4
+          ({ insertions: inserts }) => ({ ext4: inserts.ext3 + 1 }),
+          // insert 5
+          ({ insertions: inserts }) => ({ ext5: inserts.ext4 + 1 }),
+          // insert 6
+          ({ insertions: inserts }) => ({ ext6: inserts.ext5 + 1 }),
+          // insert 7
+          ({ insertions: inserts }) => ({ ext7: inserts.ext6 + 1 })
         )
-      );
-      TestBed.runInInjectionContext(() => {
-        const store = injectCraft();
-        //insert 1
-        expectTypeOf(store.userQuery.pagination).toEqualTypeOf<{
-          page: number;
-        }>();
-        expect(store.userQuery.pagination).toBeDefined();
-
-        //insert 2
-        expectTypeOf(store.userQuery.someOtherInfo).toEqualTypeOf<boolean>();
-        expect(store.userQuery.someOtherInfo).toBeDefined();
-      });
-    });
-    it('should accept seven insertions, all outputs appear in the store', () => {
-      const { injectCraft } = craft(
-        {
-          name: '',
-          providedIn: 'root',
-        },
-        craftQuery('user', () =>
-          query(
-            {
-              params: () => '5',
-              loader: async ({ params }) => {
-                return {
-                  id: params,
-                  name: 'John Doe',
-                  email: 'test@a.com',
-                } satisfies User;
-              },
-            },
-            // insert 1
-            () => ({ ext1: 1 }),
-            // insert 2
-            ({ insertions: inserts }) => ({ ext2: inserts.ext1 + 1 }),
-            // insert 3
-            ({ insertions: inserts }) => ({ ext3: inserts.ext2 + 1 }),
-            // insert 4
-            ({ insertions: inserts }) => ({ ext4: inserts.ext3 + 1 }),
-            // insert 5
-            ({ insertions: inserts }) => ({ ext5: inserts.ext4 + 1 }),
-            // insert 6
-            ({ insertions: inserts }) => ({ ext6: inserts.ext5 + 1 }),
-            // insert 7
-            ({ insertions: inserts }) => ({ ext7: inserts.ext6 + 1 })
-          )
-        )
-      );
-      TestBed.runInInjectionContext(() => {
-        const store = injectCraft();
-        expectTypeOf(store.userQuery.ext1).toEqualTypeOf<number>();
-        expectTypeOf(store.userQuery.ext2).toEqualTypeOf<number>();
-        expectTypeOf(store.userQuery.ext3).toEqualTypeOf<number>();
-        expectTypeOf(store.userQuery.ext4).toEqualTypeOf<number>();
-        expectTypeOf(store.userQuery.ext5).toEqualTypeOf<number>();
-        expectTypeOf(store.userQuery.ext6).toEqualTypeOf<number>();
-        expectTypeOf(store.userQuery.ext7).toEqualTypeOf<number>();
-        expect(store.userQuery.ext1).toBeDefined();
-        expect(store.userQuery.ext2).toBeDefined();
-        expect(store.userQuery.ext3).toBeDefined();
-        expect(store.userQuery.ext4).toBeDefined();
-        expect(store.userQuery.ext5).toBeDefined();
-        expect(store.userQuery.ext6).toBeDefined();
-        expect(store.userQuery.ext7).toBeDefined();
-      });
+      )
+    );
+    TestBed.runInInjectionContext(() => {
+      const store = injectCraft();
+      expectTypeOf(store.userQuery.ext1).toEqualTypeOf<number>();
+      expectTypeOf(store.userQuery.ext2).toEqualTypeOf<number>();
+      expectTypeOf(store.userQuery.ext3).toEqualTypeOf<number>();
+      expectTypeOf(store.userQuery.ext4).toEqualTypeOf<number>();
+      expectTypeOf(store.userQuery.ext5).toEqualTypeOf<number>();
+      expectTypeOf(store.userQuery.ext6).toEqualTypeOf<number>();
+      expectTypeOf(store.userQuery.ext7).toEqualTypeOf<number>();
+      expect(store.userQuery.ext1).toBeDefined();
+      expect(store.userQuery.ext2).toBeDefined();
+      expect(store.userQuery.ext3).toBeDefined();
+      expect(store.userQuery.ext4).toBeDefined();
+      expect(store.userQuery.ext5).toBeDefined();
+      expect(store.userQuery.ext6).toBeDefined();
+      expect(store.userQuery.ext7).toBeDefined();
     });
   });
 });
