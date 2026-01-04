47983f6a0c343499530a6acae3a822a111cb94f1
Romain <rgeffrault@dood.com>
2026-01-04 07:08:43 +0100

    test: refactor insertLocalStoragePersister tests for consistency and clarity

--- libs/ng-query/ngrx-signals/src/lib/server-state/insert-local-storage-persister.spec.ts
+++ libs/ng-query/ngrx-signals/src/lib/server-state/insert-local-storage-persister.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { asyncMethod } from './async-method';
 import { insertLocalStoragePersister } from './insert-local-storage-persister';
 import { mutation } from './mutation';
@@ -6,108 +7,105 @@ import { state } from './state';
 
 describe('insertLocalStoragePersister', () => {
   it('can be used as an insertion in a query', async () => {
-    const myQuery = query(
-      {
-        params: () => 'test',
-        loader: async () => {
-          return { data: 'testData' };
+    TestBed.runInInjectionContext(() => {
+      const myQuery = query(
+        {
+          params: () => 'test',
+          loader: async () => {
+            return { data: 'testData' };
+          },
         },
-      },
-      insertLocalStoragePersister({
-        storeName: 'myTestStore',
-        key: 'myTestQuery',
-      })
-    );
+        insertLocalStoragePersister({
+          storeName: 'myTestStore',
+          key: 'myTestQuery',
+        })
+      );
 
-    const myQuery2 = query(
-      {
-        params: () => 'test',
-        identifier: (params) => params,
-        loader: async () => {
-          return { data: 'testData' };
+      const myQuery2 = query(
+        {
+          params: () => 'test',
+          identifier: (params) => params,
+          loader: async () => {
+            return { data: 'testData' };
+          },
         },
-      },
-      insertLocalStoragePersister({
-        storeName: 'myTestStore',
-        key: 'myTestQuery2',
-      })
-    );
+        insertLocalStoragePersister({
+          storeName: 'myTestStore',
+          key: 'myTestQuery2',
+        })
+      );
+    });
   });
 
   it('can be used as an insertion in a mutation', async () => {
-    const myMutation = mutation(
-      {
-        method: () => 'test',
-        loader: async () => {
-          return { data: 'testData' };
+    TestBed.runInInjectionContext(() => {
+      const myMutation = mutation(
+        {
+          method: () => 'test',
+          loader: async () => {
+            return { data: 'testData' };
+          },
         },
-      },
-      insertLocalStoragePersister({
-        storeName: 'myTestStore',
-        key: 'myMutation',
-      })
-    );
-    const myMutation2 = mutation(
-      {
-        method: () => 'test',
-        identifier: (params) => params,
-        loader: async () => {
-          return { data: 'testData' };
+        insertLocalStoragePersister({
+          storeName: 'myTestStore',
+          key: 'myMutation',
+        })
+      );
+      const myMutation2 = mutation(
+        {
+          method: () => 'test',
+          identifier: (params) => params,
+          loader: async () => {
+            return { data: 'testData' };
+          },
         },
-      },
-      insertLocalStoragePersister({
-        storeName: 'myTestStore',
-        key: 'myMutation2',
-      })
-    );
+        insertLocalStoragePersister({
+          storeName: 'myTestStore',
+          key: 'myMutation2',
+        })
+      );
+    });
   });
 
   it('can be used as an insertion in a async method', async () => {
-    const myAsyncMethod = asyncMethod(
-      {
-        method: () => 'test',
-        loader: async () => {
-          return { data: 'testData' };
+    TestBed.runInInjectionContext(() => {
+      const myAsyncMethod = asyncMethod(
+        {
+          method: () => 'test',
+          loader: async () => {
+            return { data: 'testData' };
+          },
         },
-      },
-      insertLocalStoragePersister({
-        storeName: 'myTestStore',
-        key: 'myAsyncMethod',
-      })
-    );
-    const myAsyncMethod2 = asyncMethod(
-      {
-        method: () => 'test',
-        identifier: (params) => params,
-        loader: async () => {
-          return { data: 'testData' };
+        insertLocalStoragePersister({
+          storeName: 'myTestStore',
+          key: 'myAsyncMethod',
+        })
+      );
+      const myAsyncMethod2 = asyncMethod(
+        {
+          method: () => 'test',
+          identifier: (params) => params,
+          loader: async () => {
+            return { data: 'testData' };
+          },
         },
-      },
-      insertLocalStoragePersister({
-        storeName: 'myTestStore',
-        key: 'myAsyncMethod2',
-      })
-    );
+        insertLocalStoragePersister({
+          storeName: 'myTestStore',
+          key: 'myAsyncMethod2',
+        })
+      );
+    });
   });
 
   it('can be used as an insertion in a state', async () => {
-    const myState = state(
-      0,
-      insertLocalStoragePersister({
-        storeName: 'myTestStore',
-        key: 'myState',
-      })
-    );
-  });
-
-  it('can not be used as an insertion in a queryParams', async () => {
-    // todo
-    // const myQueryParams = queryParams(
-    //   0,
-    //   insertLocalStoragePersister({
-    //     storeName: 'myTestStore',
-    //     key: 'myState',
-    //   })
-    // );
+    TestBed.runInInjectionContext(() => {
+      const myState = state(
+        0,
+        insertLocalStoragePersister({
+          storeName: 'myTestStore',
+          key: 'myState',
+        })
+      );
+    });
   });
 });
